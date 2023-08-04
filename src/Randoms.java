8378b486d2e37c9986470cd3a0cf668fca24dd77
Darya <streko3ka@gmail.com>
2023-08-04 11:47:57 +0300

    Initial commit

---
+++ src/Randoms.java
@@ -0,0 +1,30 @@
+import java.util.Iterator;
+import java.util.Random;
+
+public class Randoms implements Iterable<Integer> {
+    protected Random random;
+    protected int min;
+    protected int max;
+
+
+    public Randoms(int min, int max) {
+        this.max = max;
+        this.min = min;
+        random = new Random();
+    }
+
+    @Override
+    public Iterator<Integer> iterator() {
+        return new Iterator<>() {
+            @Override
+            public boolean hasNext() {
+                return true;
+            }
+
+            @Override
+            public Integer next() {
+                return random.nextInt(max - min + 1) + min;
+            }
+        };
+    }
+}
